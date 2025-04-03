9fc444b1537166699641eebac00c95dad48a1df3
Christian Helle <christian.helle@outlook.com>
2025-04-04 01:20:17 +0200

    Refactor RequestBodyTests and update project structure
    Modified `RequestBodyTests` to use `[Theory]` with multiple `[InlineData]` for enhanced testing of HTTP request parsing. Updated the solution file to reorganize test projects, adding `HttpTestGen.Tests` to the `/Tests/` folder.

--- src/HttpTestGen.SourceGenerator.Tests/Parser/RequestBodyTests.cs
+++ src/HttpTestGen.SourceGenerator.Tests/Parser/RequestBodyTests.cs
@@ -4,24 +4,119 @@ namespace HttpTestGen.SourceGenerator.Tests.Parser;
 
 public class RequestBodyTests
 {
-    [Fact]
-    public void Parse_Body_Request()
+    [Theory]
+    [InlineData(
+        """
+        GET https://localhost/ HTTP/1.1
+        Accept: application/json
+        {
+          "id": 1234,
+          "type": "message",
+          "payload": {
+            "name": "test"
+          }
+        }
+        """
+    )]
+    [InlineData(
+        """
+        GET https://localhost/ HTTP/1.1
+        Accept: application/json
+
+        {
+          "id": 1234,
+          "type": "message",
+          "payload": {
+            "name": "test"
+          }
+        }
+        """
+    )]
+    [InlineData(
+        """
+        GET https://localhost/ HTTP/1.1
+        Accept: application/json
+
+        {
+          "id": 1234,
+          "type": "message",
+          "payload": {
+            "name": "test"
+          }
+        }
+
+        """
+    )]
+    [InlineData(
+        """
+        #
+        GET https://localhost/ HTTP/1.1
+        Accept: application/json
+
+        {
+          "id": 1234,
+          "type": "message",
+          "payload": {
+            "name": "test"
+          }
+        }
+        """
+    )]
+    [InlineData(
+        """
+        ##
+        #
+        GET https://localhost/ HTTP/1.1
+        Accept: application/json
+
+        {
+          "id": 1234,
+          "type": "message",
+          "payload": {
+            "name": "test"
+          }
+        }
+        """
+    )]
+    [InlineData(
+        """
+        GET https://localhost/ HTTP/1.1
+        Accept: application/json
+
+
+        {
+          "id": 1234,
+          "type": "message",
+          "payload": {
+            "name": "test"
+          }
+        }
+        #
+        """
+    )]
+    [InlineData(
+        """
+        #
+
+        #
+
+        GET https://localhost/ HTTP/1.1
+        Accept: application/json
+        
+        {
+          "id": 1234,
+          "type": "message",
+          "payload": {
+            "name": "test"
+          }
+        }
+        #
+        """
+    )]
+    public void Parse_Body_Request(string content)
     {
         var sut = new HttpFileParser();
-        var requests = sut.Parse(
-            """
-            GET https://localhost/ HTTP/1.1
-            Accept: application/json
-
-            {
-              "id": 1234,
-              "type": "message",
-              "payload": {
-                "name": "test"
-              }
-            }
-            """
-        ).ToList();
+        var requests = sut.Parse(content).ToList();
 
         var first = requests.Single();
         Assert.Equal("GET", first.Method);
