58b3277b553bcd058e4a771a349c23345c2572a3
Christian Helle <christian.helle@outlook.com>
2025-04-04 08:48:52 +0200

    Refactor HeaderTests to use parameterized tests
    Updated the `Parse_Headers_Request` method in the `HeaderTests` class to utilize `[Theory]` and `[InlineData]` attributes for parameterized testing. This change allows multiple header string variations to be tested within a single method, enhancing test coverage and flexibility. The method now accepts a `string content` parameter, which is passed to the `sut.Parse` method.

--- src/HttpTestGen.SourceGenerator.Tests/Parser/HeaderTests.cs
+++ src/HttpTestGen.SourceGenerator.Tests/Parser/HeaderTests.cs
@@ -4,18 +4,47 @@ namespace HttpTestGen.SourceGenerator.Tests.Parser;
 
 public class HeaderTests
 {
-    [Fact]
-    public void Parse_Headers_Request()
+    [Theory]
+    [InlineData(
+        """
+        GET https://localhost/ HTTP/1.1
+        Accept: application/json
+        Authorization: Bearer token
+        x-custom-header: custom value
+        """)]
+    [InlineData(
+        """
+        #
+        GET https://localhost/ HTTP/1.1
+        Accept: application/json
+        Authorization: Bearer token
+        x-custom-header: custom value
+        """)]
+    [InlineData(
+        """
+        #
+        GET https://localhost/ HTTP/1.1
+        Accept: application/json
+        Authorization: Bearer token
+        x-custom-header: custom value
+        #
+        """)]
+    [InlineData(
+        """
+        
+        #
+
+        GET https://localhost/ HTTP/1.1
+        Accept: application/json
+        Authorization: Bearer token
+        x-custom-header: custom value
+        
+        
+        """)]
+    public void Parse_Headers_Request(string content)
     {
         var sut = new HttpFileParser();
-        var requests = sut.Parse(
-            """
-            GET https://localhost/ HTTP/1.1
-            Accept: application/json
-            Authorization: Bearer token
-            x-custom-header: custom value
-            """
-        );
+        var requests = sut.Parse(content).ToList();
 
         var first = requests.Single();
         Assert.Equal("GET", first.Method);
