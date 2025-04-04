417c6bee75b31aee136c98552b8154fec5060a84
Christian Helle <christian.helle@outlook.com>
2025-04-04 15:59:44 +0200

    Add test cases for 404 status code in header assertions

--- src/HttpTestGen.SourceGenerator.Tests/Parser/AssertionsTests.cs
+++ src/HttpTestGen.SourceGenerator.Tests/Parser/AssertionsTests.cs
@@ -8,12 +8,69 @@ public class AssertionsTests
         GET https://localhost/notfound
         EXPECTED_STATUS: 404
         """)]
+    [Arguments(
+        """
+        GET https://localhost/notfound
+
+        EXPECTED_STATUS: 404
+        """)]
+    [Arguments(
+        """
+        GET https://localhost/notfound
+        Accept: application/json
+        EXPECTED_STATUS: 404
+        """)]
+    [Arguments(
+        """
+        GET https://localhost/notfound
+        Accept: application/json
+
+        EXPECTED_STATUS: 404
+        """)]
     public async Task Parse_Assertions(string content)
     {
         var sut = new HttpFileParser();
         var requests = sut.Parse(content).ToList();
 
         var first = requests.Single();
+
+        await Assert
+            .That(first.Assertions.ExpectedStatusCode)
+            .IsEqualTo(404);
+    }
+    [Test]
+    [Arguments(
+        """
+        GET https://localhost/notfound
+        Accept: application/json
+        EXPECTED_STATUS: 404
+        """)]
+    [Arguments(
+        """
+        GET https://localhost/notfound
+        Accept: application/json
+
+        EXPECTED_STATUS: 404
+        """)]
+    public async Task Parse_Header_Assertions(string content)
+    {
+        var sut = new HttpFileParser();
+        var requests = sut.Parse(content).ToList();
+
+        var first = requests.Single();
+
+        await Assert
+            .That(first.Method)
+            .IsEqualTo("GET");
+        await Assert
+            .That(first.Endpoint)
+            .IsEqualTo("https://localhost/notfound");
+        await Assert
+            .That(first.Headers.ContainsKey("Accept"))
+            .IsTrue();
+        await Assert
+            .That(first.Headers["Accept"])
+            .IsEqualTo("application/json");
         await Assert
             .That(first.Assertions.ExpectedStatusCode)
             .IsEqualTo(404);
