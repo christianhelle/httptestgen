1f1bc77a86229e0690b95acfa63521f3c7bbc722
Christian Helle <christian.helle@outlook.com>
2025-04-05 01:01:59 +0200

    Enhance HTTP file parsing with regex for request and header extraction

--- src/HttpTestGen.SourceGenerator/HttpFileParser.cs
+++ src/HttpTestGen.SourceGenerator/HttpFileParser.cs
@@ -1,98 +1,99 @@
 ﻿using System.Collections.Specialized;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace HttpTestGen.SourceGenerator;
 
 public class HttpFileParser
 {
+    private static readonly Regex RegexUrl = new(
+        @"^((?<method>get|post|patch|put|delete|head|options|trace))\s*(?<url>[^\s]+)\s*(?<version>HTTP/.*)?",
+        RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+    private static readonly Regex RegexHeader = new(
+        @"^(?<name>[^\s]+)?([\s]+)?(?<operator>:)(?<value>.+)",
+        RegexOptions.Compiled);
+
     public IEnumerable<HttpFileRequest> Parse(string content)
     {
+        HttpFileRequest? httpFileRequest = null;
+        StringBuilder body = new();
+
         var lines = content.Split('\n');
-        for (int i = 0; i < lines.Length; i++)
+        for (var i = 0; i < lines.Length; i++)
         {
             var line = lines[i].Trim();
-            if (line.StartsWith("#"))
+            if (line.StartsWith("#") || string.IsNullOrWhiteSpace(line))
             {
                 continue;
             }
 
-            var requestLine = line
-                .TrimStart()
-                .Split([" "], StringSplitOptions.RemoveEmptyEntries);
-            if (requestLine.Length < 2)
+            var match = RegexUrl.Match(line);
+            if (match.Success)
             {
+                if (httpFileRequest is not null)
+                {
+                    yield return httpFileRequest;
+                }
+                
+                httpFileRequest = new()
+                {
+                    Method = match.Groups["method"].Value.Trim().ToUpperInvariant(),
+                    Endpoint = match.Groups["url"].Value.Trim(),
+                };
                 continue;
             }
 
-            var requestStartIndex = i;
-            var method = requestLine[0];
-            var endpoint = requestLine[1];
-
-            var headers = new StringDictionary();
-            for (int j = i + 1; j < lines.Length; j++)
+            match = RegexHeader.Match(line);
+            if (match.Success && httpFileRequest is not null)
             {
-                if (lines[j].StartsWith("#"))
-                {
-                    break;
-                }
-
-                var split = lines[j].Split(':');
-                if (split.Length == 2)
-                {
-                    headers[split[0].Trim()] = split[1].Trim();
-                    i++;
-                }
-                else
-                {
-                    break;
-                }
+                httpFileRequest!.Headers.Add(
+                    match.Groups["name"].Value.Trim(),
+                    match.Groups["value"].Value.Trim());
+                continue;
             }
 
-            StringBuilder body = new();
-            for (int j = i + 1; j < lines.Length; j++)
+            if (line.Contains("EXPECTED_RESPONSE_STATUS") && httpFileRequest is not null)
             {
-                if (lines[j].StartsWith("#"))
-                {
-                    break;
-                }
-
-                body.Append(lines[j]);
-                i++;
+                var value = line.Split(' ')[1].Trim();
+                httpFileRequest!.Assertions.ExpectedStatusCode = int.Parse(value);
+                continue;
             }
 
-            var assertions = new HttpRequestAssertions();
-            for (var j = requestStartIndex + 1; j < lines.Length; j++)
+            if (line.Contains("EXPECTED_RESPONSE_HEADER") && httpFileRequest is not null)
             {
-                line = lines[j];
-                if (line.StartsWith("#"))
+                var header = line.Substring("EXPECTED_RESPONSE_HEADER".Length).Trim();
+                var headerParts = header.Split(':');
+                if (headerParts.Length != 2)
+                {
                     continue;
-
-                if (line.StartsWith("EXPECTED_RESPONSE_STATUS", StringComparison.OrdinalIgnoreCase))
-                {
-                    var split = line.Split(':');
-                    if (split.Length == 2)
-                    {
-                        if (int.TryParse(split[1], out var statusCode))
-                            assertions.ExpectedStatusCode = statusCode;
-                    }
                 }
 
-                if (line.StartsWith("EXPECTED_RESPONSE_HEADER", StringComparison.OrdinalIgnoreCase))
-                {
-                    var split = line.Split(':');
-                    if (split.Length == 3) 
-                        assertions.ExpectedHeaders[split[1].Trim()] = split[2].Trim();
-                }
+                var headerName = headerParts[0].Trim();
+                var headerValue = headerParts[1].Trim();
+                httpFileRequest!.Assertions.ExpectedHeaders.Add(headerName, headerValue);
+                continue;
             }
 
-            yield return new HttpFileRequest
+            for (var j = i + 1; j < lines.Length; j++)
             {
-                Method = method,
-                Endpoint = endpoint.Trim(),
-                Headers = headers,
-                RequestBody = body.ToString().Trim(),
-                Assertions = assertions
-            };
+                line = lines[j].Trim();
+                if (line.StartsWith("#") || string.IsNullOrWhiteSpace(line))
+                    continue;
+                body.AppendLine(line);
+                if (RegexUrl.Match(line).Success)
+                    break;
+            }
+
+            if (httpFileRequest == null) continue;
+            if (body.Length > 0) 
+                httpFileRequest.RequestBody = body.ToString();
+            yield return httpFileRequest;
+            httpFileRequest = null;
+            body.Clear();
         }
+
+        if (httpFileRequest != null)
+            yield return httpFileRequest;
     }
 }
