5c38a99edec226766d05bee2fe567601282ec5e0
Christian Helle <christian.helle@outlook.com>
2025-04-04 08:57:57 +0200

    Move HttpFileRequest to its own file

--- src/HttpTestGen.SourceGenerator/HttpFileParser.cs
+++ src/HttpTestGen.SourceGenerator/HttpFileParser.cs
@@ -68,12 +68,3 @@ public class HttpFileParser
         }
     }
 }
-
-public class HttpFileRequest
-{
-    public string Method { get; set; } = null!;
-    public string Endpoint { get; set; } = null!;
-    public string? HttpVersion { get; set; }
-    public string? RequestBody { get; set; }
-    public StringDictionary Headers { get; set; } = [];
-}
---
+++ src/HttpTestGen.SourceGenerator/HttpFileRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Specialized;
+
+namespace HttpTestGen.SourceGenerator;
+
+public class HttpFileRequest
+{
+    public string Method { get; set; } = null!;
+    public string Endpoint { get; set; } = null!;
+    public string? HttpVersion { get; set; }
+    public string? RequestBody { get; set; }
+    public StringDictionary Headers { get; set; } = [];
+}
